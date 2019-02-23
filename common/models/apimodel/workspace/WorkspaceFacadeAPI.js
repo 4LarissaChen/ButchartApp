89cda7113c21f6efd1590842f2e10caa407ffb7c
Miruako <zzf1215225lx@hotmail.com>
2019-02-23 09:33:30 +0800

    add apis

--- common/models/apimodel/workspace/WorkspaceFacadeAPI.js
+++ common/models/apimodel/workspace/WorkspaceFacadeAPI.js
@@ -42,4 +42,52 @@ module.exports = function (WorkspaceFacadeAPI) {
       cb(err, null);
     })
   }
+
+  WorkspaceFacadeAPI.remoteMethod('createOrder', {
+    description: "Create an order.",
+    accepts: [{ arg: 'customerId', type: 'string', required: true, description: "Customer Id.", http: { source: 'path' } },
+    { arg: 'orderParams', type: 'CreateOrderRequest', required: true, description: "detail order properties", http: { source: 'body' } }],
+    returns: { arg: 'isSuccess', type: 'IsSuccessResponse', description: "", root: true },
+    http: { path: '/workspace/customerId/:customerId/createOrder', verb: 'put', status: 200, errorStatus: 500 }
+  });
+
+  WorkspaceFacadeAPI.createOrder = function (customerId, orderParams, cb) {
+    var CustomerMicroService = loopback.findModel("CustomerMicroService");
+    var OrderMicroService = loopback.findModel("OrderMicroService");
+    let orderId;
+    return OrderMicroService.OrderAPI_createOrder({ customerId: customerId, productList: orderParams.productList }).then(result => {
+      orderId = result.obj.orderId;
+      return CustomerMicroService.TransactionAPI_createTransaction({
+        customerId: customerId,
+        orderId: orderId,
+        storeId: orderParams.storeId,
+        addressId: orderParams.addressId
+      }).then(() => {
+        cb(null, { isSuccess: true });
+      }).catch(err => {
+        cb(err.null);
+      })
+
+    })
+  }
+
+  WorkspaceFacadeAPI.remoteMethod('payOrder', {
+    description: "Pay an order.",
+    accepts: [{ arg: 'customerId', type: 'string', required: true, description: "Customer Id.", http: { source: 'path' } },
+    { arg: 'transactionId', type: 'string', required: true, description: "Transaction Id", http: { source: 'path' } }],
+    returns: { arg: 'isSuccess', type: 'IsSuccessResponse', description: "", root: true },
+    http: { path: '/workspace/customerId/:customerId/transactionId/:transactionId/payOrder', verb: 'put', status: 200, errorStatus: 500 }
+  });
+  WorkspaceFacadeAPI.payOrder = function (customerId, transactionId, cb){
+    var CustomerMicroService = loopback.findModel("CustomerMicroService");
+    var OrderMicroService = loopback.findModel("OrderMicroService");
+    //TBD third part pay.
+    return Promise.resolve().then(result => {
+      return CustomerMicroService.TransactionAPI_changeStatus({transactionId: transactionId, status: "payed"})
+    }).then(() => {
+      cb(null, {isSuccess: true});
+    }).catch( err => {
+      cb(err, null);
+    })
+  }
 }
