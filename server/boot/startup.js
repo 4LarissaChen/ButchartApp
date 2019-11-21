7dd7515f6e690b3ee271c877a4875952a250416e
miko <zzfzhang@cn.ibm.com>
2019-11-22 00:53:14 +0800

    batch job open

--- server/boot/startup.js
+++ server/boot/startup.js
@@ -83,26 +83,26 @@ var getWXAccessTokenBatchJob = function () {
 }
 
 module.exports = function (app) {
-  // startBatchAssignJob();
-  // startStatisticsBatchJob();
-  // starlocationBatchJob();
-  // getWXAccessTokenBatchJob();
-  // let wechatPayService = new WechatPayService();
-  // let UserMicroService = loopback.findModel("UserMicroService");
-  // return wechatPayService.getAccessToken().then(result => {
-  //   console.log(moment().local().format('YYYY-MM-DD HH:mm:ss') + ": " + result.access_token);
-  //   global.settings.wxConfig = { access_token: result.access_token };
-  //   console.log("access_token: " + global.settings.wxConfig.access_token + " at " + moment().local().format('YYYY-MM-DD HH:mm:ss'));
-  //   let url = "https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token=" + global.settings.wxConfig.access_token + "&type=jsapi";
-  //   let option = {
-  //     method: 'GET',
-  //     url: url
-  //   };
-  //   return rp(option).then(result => {
-  //     result = JSON.parse(result);
-  //     global.settings.wxConfig["jsapi_ticket"] = result.ticket;
-  //     console.log("jsapi_ticket: " + global.settings.wxConfig.jsapi_ticket + " at " + moment().local().format('YYYY-MM-DD HH:mm:ss'));
-  //     return UserMicroService.SystemAPI_upsertAccessToken({ access_token: global.settings.wxConfig.access_token ? global.settings.wxConfig.access_token : "access_token" });
-  //   });
-  // })
+  startBatchAssignJob();
+  startStatisticsBatchJob();
+  starlocationBatchJob();
+  getWXAccessTokenBatchJob();
+  let wechatPayService = new WechatPayService();
+  let UserMicroService = loopback.findModel("UserMicroService");
+  return wechatPayService.getAccessToken().then(result => {
+    console.log(moment().local().format('YYYY-MM-DD HH:mm:ss') + ": " + result.access_token);
+    global.settings.wxConfig = { access_token: result.access_token };
+    console.log("access_token: " + global.settings.wxConfig.access_token + " at " + moment().local().format('YYYY-MM-DD HH:mm:ss'));
+    let url = "https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token=" + global.settings.wxConfig.access_token + "&type=jsapi";
+    let option = {
+      method: 'GET',
+      url: url
+    };
+    return rp(option).then(result => {
+      result = JSON.parse(result);
+      global.settings.wxConfig["jsapi_ticket"] = result.ticket;
+      console.log("jsapi_ticket: " + global.settings.wxConfig.jsapi_ticket + " at " + moment().local().format('YYYY-MM-DD HH:mm:ss'));
+      return UserMicroService.SystemAPI_upsertAccessToken({ access_token: global.settings.wxConfig.access_token ? global.settings.wxConfig.access_token : "access_token" });
+    });
+  })
 }
